085a190ecddcd164d05cd4e4baba07eab79a6040
neekgreen <neekgreen@outlook.com>
2017-11-20 07:27:20 -0600

    initial commit

---
+++ tests/SimpleMeasure.IntegrationTests/ValuesController.cs
@@ -0,0 +1,19 @@
+﻿namespace SimpleMeasure.IntegrationTests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc;
+    using SimpleMeasure;
+
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        [Benchmark, HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
