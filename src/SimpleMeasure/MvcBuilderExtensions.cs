3e3ecde6d3618958ceddb0eefc42431aefc4b50c
Nicholas Myers <neekgreen@users.noreply.github.com>
2017-11-24 10:29:59 -0600

    mvc extensions support

---
+++ src/SimpleMeasure/MvcBuilderExtensions.cs
@@ -0,0 +1,13 @@
+namespace Microsoft.Extensions.DependencyInjection
+{
+    using SimpleMeasure;
+    
+    public static class MvcBuilderExtensions
+    {
+        public static IMvcBuilder UseBenchmark(this IMvcBuilder builder)
+        {
+            return 
+                builder.AddMvcOptions(t => t.Filters.Add(typeof(BenchmarkAttribute)));
+        }
+    }
+}
---
+++ src/SimpleMeasure/MvcCoreBuilderExtensions.cs
@@ -0,0 +1,13 @@
+namespace Microsoft.Extensions.DependencyInjection
+{
+    using SimpleMeasure;
+    
+    public static class MvcCoreBuilderExtensions
+    {
+        public static IMvcCoreBuilder UseBenchmark(this IMvcCoreBuilder builder)
+        {
+            return 
+                builder.AddMvcOptions(t => t.Filters.Add(typeof(BenchmarkAttribute)));
+        }
+    }
+}
