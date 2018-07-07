58c5ef4077d227b941149d8b5833ec75dc7c1ccb
neekgreen <neekgreen@users.noreply.github.com>
2018-07-07 17:46:56 -0500

    target dotnet core 2.1

--- src/SimpleMeasure/BenchmarkAttribute.cs
+++ src/SimpleMeasure/BenchmarkAttribute.cs
@@ -2,7 +2,6 @@
 {
     using System.Diagnostics;
     using System.Threading.Tasks;
-    using Microsoft.AspNetCore.Mvc;
     using Microsoft.AspNetCore.Mvc.Filters;
     
     public class BenchmarkAttribute : ActionFilterAttribute
